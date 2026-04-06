661bc93320237b5bf1d8b5bed18c8bac375f2b1c
Mohammed <mohammed50atia@gmail.com>
2026-04-06 16:13:23 +0200

    Solving Error in config/senctum abut the path of the imported file

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,16 +6,17 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias([
             'role' => \App\Http\Middleware\CheckRole::class,
+
         ]);
-        
+        $middleware->statefulApi();
         $middleware->web(append: [
             \App\Http\Middleware\HandleInertiaRequests::class,
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
@@ -67,7 +68,7 @@ return Application::configure(basePath: dirname(__DIR__))
                     'success' => false,
                     'message' => 'Server error: ' . $e->getMessage(),
                 ];
-                
+
                 // Do not expose sensitive info in production
                 if (config('app.env') === 'production') {
                     $response['message'] = 'Server error';
